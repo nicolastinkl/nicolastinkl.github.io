4a82f7badfccc7ded1aac3d48ce3972b640a1e66
zeus <zeus@gmail.com>
2023-03-22 10:57:30 +0800

    Commit code. Update time: 2023-03-22

--- git.sh
+++ git.sh
@@ -3,9 +3,11 @@ cd zeus-blog
 hexo clean
 hexo g
 cd ..
-
+cp -r zeus-blog/public ./
 git add .
 date +%F > timefile
 currentTime=$(<timefile)
 git commit -m "Commit code. Update time: $currentTime "
 git push
+
+echo "https://nicolastinkl.github.io/"
